284ec43e5273ac5c528d7080ef2449f458360fc2
Paul <paul.ngugi@student.moringaschool.com>
2022-12-15 01:56:58 +0300

    build a list that will hold quotes cards

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 //DOM render functions
 function renderOneQuote(quote) {
-    
+    //build quote
+    let card = document.createElement('li')
+    card.innerHTML = `
+    <div class="card" style="width: 20rem;">
+              <div class="card-body">
+                <blockquote class="blockquote mb-0">
+                    <p>${quote.text}</p>
+                    <footer class="blockquote-footer">Quoted by <cite title="Source Title">${quote.author}</cite></footer>
+                </blockquote>
+                <br>
+                <button id="deleteBTN" class="btn btn-outline-info">Delete</button>
+              </div>
+            </div>
+
+            
+    `
 }
